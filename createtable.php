ef77b281c86a052ef90ac9793a18a4484792baf4
omar ihab <omar ihab>
2016-04-04 21:12:33 +0200

    'createtable.php'

--- createtable.php
+++ createtable.php
@@ -5,12 +5,16 @@ use Silex\Application;
 
 $app = new Application();
 $dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new PdoServiceProvider(),
-               array(
-                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-                   'pdo.username' => $dbopts["user"],
-                   'pdo.password' => $dbopts["pass"]
-               )
+$app->register(
+    new PdoServiceProvider(),
+    array(
+        'pdo.dsn' => 'pdo_mysql:dbname=test;host=127.0.0.1',
+        'pdo.username' => 'username', // optional
+        'pdo.password' => 'password', // optional
+        'pdo.options' => array( // optional
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
+        )
+    )
 );
 
 ?>
