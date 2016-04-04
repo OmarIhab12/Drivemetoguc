1e0c72d8ec7a5e2bd3ffdb98777ff020ca7d440d
omar ihab <omar ihab>
2016-04-04 21:03:01 +0200

    'createtable.php'

--- createtable.php
+++ createtable.php
@@ -1,9 +1,15 @@
 <?php
 
-$app->get('/cowsay', function() use($app) {
-  $app['monolog']->addDebug('cowsay');
-  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
-});
+use Herrera\Pdo\PdoServiceProvider;
+use Silex\Application;
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+               array(
+                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                   'pdo.username' => $dbopts["user"],
+                   'pdo.password' => $dbopts["pass"]
+               )
+);
 
 
 ?>
