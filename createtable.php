a62217752c7124b0659f992e302adb2ba2ce4033
omar ihab <omar ihab>
2016-04-04 21:04:28 +0200

    'createtable.php'

--- createtable.php
+++ createtable.php
@@ -3,13 +3,6 @@
 use Herrera\Pdo\PdoServiceProvider;
 use Silex\Application;
 $dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new Herrera\Pdo\PdoServiceProvider(),
-               array(
-                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-                   'pdo.username' => $dbopts["user"],
-                   'pdo.password' => $dbopts["pass"]
-               )
-);
 
 
 ?>
