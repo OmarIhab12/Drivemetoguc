ea51598eb717e084f5b55f2018a4caca681d1557
omar ihab <omar ihab>
2016-04-12 12:17:32 +0200

    adding database username an dpassword

--- createtable.php
+++ createtable.php
@@ -3,16 +3,13 @@
 use Herrera\Pdo\PdoServiceProvider;
 use Silex\Application;
 
-$app = new Application();
 $dbopts = parse_url(getenv('DATABASE_URL'));
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new PdoServiceProvider(),
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
                array(
                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-                   'pdo.username' => $dbopts["user"],
-                   'pdo.password' => $dbopts["pass"]
+                   'pdo.username' => $dbopts["jgnzsrfulamkrq"],
+                   'pdo.password' => $dbopts["WBBl9gjBGfXJYE2wh0nvk_smTq"]
                )
 );
-echo "yes" ;
 
 ?>
